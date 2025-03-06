d2e472420b0f1c847f2ae8fbcdf07bf2dfde63d3
talalzubair123 <talalmirza317@gmail.com>
2025-03-06 09:52:39 +0500

    Create calculator.cs

---
+++ calculator.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Name:Talal Zubair");
+        Console.WriteLine("Roll Number: 24-ST-086");
+    }
+}
