ccafaa2a7e56d204c99daeeebd4ee967fb8894c6
talalzubair123 <talalmirza317@gmail.com>
2025-03-06 09:55:03 +0500

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -6,5 +6,11 @@ class Program
     {
         Console.WriteLine("Name:Talal Zubair");
         Console.WriteLine("Roll Number: 24-ST-086");
+    
+        int a, b;
+        Console.Write("Enter two numbers: ");
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Sum: {a + b}");
     }
 }
