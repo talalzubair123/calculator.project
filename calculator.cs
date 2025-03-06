1cdfdf3429d8b188d50f40343729702cf13ea2fa
talalzubair123 <talalmirza317@gmail.com>
2025-03-06 09:59:51 +0500

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -6,5 +6,15 @@ class Program
     {
         Console.WriteLine("Name:Talal Zubair");
         Console.WriteLine("Roll Number: 24-ST-086");
+  
+        int a, b;
+        Console.Write("Enter two numbers: ");
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+
+        if (b != 0)
+            Console.WriteLine($"Quotient: {a / b}");
+        else
+            Console.WriteLine("Cannot divide by zero!");
     }
 }
